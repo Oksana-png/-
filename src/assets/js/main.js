79cd2cda32e55468ddd2689f6d5c1ff31fe9721d
Oksana-png <oksana_ovchinnikova@inbox.ru>
2023-04-08 02:05:10 +0500

    fix and create btn-up and more

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,3 +3,22 @@ const btnBurger = document.querySelector('.burger-menu__btn ');
 btnBurger.addEventListener('click', () => {
   btnBurger.classList.toggle('active');
 });
+
+const btnUp = document.querySelector('.btn-up');
+
+btnUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+});
+
+window.addEventListener('scroll', () => {
+  console.log(window);
+  if (window.innerHeight < window.scrollY) {
+    btnUp.style.display = 'flex';
+  } else {
+    btnUp.style.display = 'none';
+  }
+});
